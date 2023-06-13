9bf4dc0428cae1fb5fce3606d990d9d9332d3696
Furqon Wilogo <fdwilogo@gmail.com>
2023-06-13 04:50:29 +0300

    delete test

--- src/graphql/Engagement/query/index.ts
+++ src/graphql/Engagement/query/index.ts
@@ -2,4 +2,3 @@ export * from './sdl/pushNewUserToNotion';
 export * from './sdl/sendNewUserEmail';
 export * from './sdl/sendInvitationEmailFromUser';
 export * from './sdl/pushSupportTicket';
-// export * from './sdl/test';
